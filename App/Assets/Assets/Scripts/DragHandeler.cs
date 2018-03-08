070f869554384cb47285bdc90bdc5138ca11214f
Zuka98 <32645506+Zuka98@users.noreply.github.com>
2018-03-08 12:03:29 +0000

    Microsoft Table 1.0 Application
    4 players, TIming, Menu, UI

---
+++ App/Assets/Assets/Scripts/DragHandeler.cs
@@ -0,0 +1,48 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.EventSystems;
+
+//public class DragHandeler : MonoBehaviour, IBeginDragHandler , IDragHandler, IEndDragHandler
+//{
+//    public static GameObject itemBeingDragged;
+//    Vector3 startPosition;
+
+//    #region IBeginDragHandler implementation    
+
+//    public void onBeingDrag(PointerEventData eventData)
+//    {
+//        itemBeingDragged = gameObject;
+//        startPosition = transform.position;
+//        startParent = transform.parent;
+//        GetComponent<CanvasGroup>().blocksRaycasts = false;
+//    }
+
+//    #endregion
+
+//    #region IDragHandler Implementation
+
+//    public void OnDrag(PointerEventData eventData)
+//    {
+//        throw new System.NotImplementedException();
+//    }
+
+//    #endregion
+
+//    #region IEndDragHandler implementation
+
+//    public void OnEndDrag(PointerEventData eventData)
+//    {
+//        itemBeingDragged = null;
+//        GetComponent<CanvasGroup>().blocksRaycasts = true;
+//        if (transform.parent != startParent)
+//        {
+//            transform.position = startPosition;
+//        }
+//    }
+
+
+//    #endregion
+
+
+//}
---
+++ App/Assets/Assets/Scripts/SubmitToMenu.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SubmitToMenu : MonoBehaviour {
+
+	public void Button_Click(){
+		SceneManager.LoadScene("MenuScene");
+	}
+}
