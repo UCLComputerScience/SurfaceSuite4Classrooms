5ccf87a33b2ab44cbd1555ff604704288a41ac77
Adam Peace <adam.n.peace@gmail.com>
2018-03-15 11:52:06 +0000

    Added charts, start still in progress

--- api/routes/index.js
+++ api/routes/index.js
@@ -4,19 +4,36 @@ var app = express()
 app.get('/', function(req, res) {
     // render to views/index.ejs template file
     if (1!=1) {
+        console.log('Served signin')
         res.render('signin')
     } else {
+        console.log('Served Dashboard')
         res.redirect('/dashboard')
     }
     
 })
 
-app.get('/startgame', function(req, res) {
-    // render to views/index.ejs template file
-    res.render('startgame')
-    
+
+app.get('/startgame', function (req, res, next) {
+    req.getConnection(function (error, conn) {
+        conn.query('SELECT * FROM student ORDER BY last_name ASC;', function (err, rows, fields) {
+            //if(err) throw err
+            console.log("There were", rows.length, 'rows found for get request')
+            if (err) {
+                req.flash('error', err)
+                res.render('startgame', {
+                    data: '',
+                    output: ''
+                })
+            } else {
+                res.render('startgame', {
+                    data: rows,
+                    output: ''
+                })
+            }
+        })
+    })
 })
- 
 /** 
  * We assign app object to module.exports
  * 
